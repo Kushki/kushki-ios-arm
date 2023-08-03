38824bed351384809f33dfef2743e91bf09273e7
Javier Sigüenza <javier@Javiers-MacBook-Pro.local>
2023-08-03 12:47:36 -0500

    install and update

---
+++ Example/Pods/Target Support Files/kushki-ios-arm/kushki-ios-arm-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_kushki_ios_arm : NSObject
+@end
+@implementation PodsDummy_kushki_ios_arm
+@end
---
+++ Example/Pods/Target Support Files/kushki-ios-arm/kushki-ios-arm-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double kushki_ios_armVersionNumber;
+FOUNDATION_EXPORT const unsigned char kushki_ios_armVersionString[];
+
